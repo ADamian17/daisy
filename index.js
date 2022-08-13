9b068559293f58417301f767288f79536c66a5f1
Adonis Martin <adonisdesuvikdo@gmail.com>
2022-08-13 16:09:07 -0700

    added guard

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ const { clear, debug } = flags;
 (async () => {
 	init({ clear });
 	input.includes(`help`) && cli.showHelp(0);
+	const userChoice = input[0];
 
-	daisy.generate(input[0]);
+	if (!userChoice) {
+		return console.log('Please run `daisy help` to see all option');
+	}
+
+	daisy.generate(userChoice);
 
 	debug && log(flags);
 })();

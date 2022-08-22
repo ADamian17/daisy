37996fcf59c1dd9c5796e2f3aa643eb4649deddb
Adonis Martin <adonisdesuvikdo@gmail.com>
2022-08-21 17:33:59 -0700

    remove unused modules

--- utils/jestGlobalSetup.js
+++
@@ -1,7 +0,0 @@
-const { mkdir } = require('../utils');
-
-module.exports = async () => {
-	await mkdir('src', 'components');
-	await mkdir('src', 'containers');
-	await mkdir('src', 'templates');
-};

0da8b58519dcecfb9762a466269ae734ee02949a
Adonis D Martin <adonisdesuvikdo@gmail.com>
2022-09-01 09:01:07 -0700

    Update component for shared dir (#9)

--- app/Daisy.js
+++ app/Daisy.js
@@ -4,12 +4,17 @@ const { mainDirPaths } = require('../config');
 class Daisy {
 	async generate(userChoice) {
 		const userChoiceMap = createUserChoiceMap(userChoice);
+
+		if (!mainDirPaths.get(userChoiceMap.get('task'))) {
+			return console.log('Please run `daisy help` to see all option');
+		}
+
 		const current = mainDirPaths.get(userChoiceMap.get('task'));
 		const baseDirPath = current?.baseDirPath;
 		const isValidPath = await validateBaseDirPath(baseDirPath);
 
 		if (isValidPath) {
-			current.taskInit();
+			current.taskInit(userChoiceMap.get('nested-dir'));
 		} else {
 			process.exit();
 		}

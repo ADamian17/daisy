4843cf124aadbc91fb9af6ed214c6f01762c4549
ADamian17 <adonisdesuvikdo@gmail.com>
2023-05-07 13:48:07 -0700

    remove unnecessary log

--- index.js
+++ index.js
@@ -5,23 +5,22 @@
  * @author Adonis D Martin <a>
  */
 
-const { init, cli, log } = require('./utils');
+const { init, cli } = require('./utils');
 const { daisy } = require('./app');
 
-const commands = cli.input;
+const command = cli.input;
 const flags = cli.flags;
 const { clear } = flags;
 
 (async () => {
 	init({ clear });
-	console.log('help', flags.help);
 	flags.help && cli.showHelp(0);
-	const cmd = commands.at(0);
+
+	const cmd = command.at(0);
 
 	if (!cmd) {
-		return console.log('Please run `daisy help` to see all option');
+		return console.log('Please run `daisy --help` to see all option');
 	}
 
-	console.log({ cmd });
 	// daisy.generate(userChoice[0], cli.flags);
 })();

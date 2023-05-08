b66082d361f4b41c8cb3bbd7480d654e0891a43d
ADamian17 <adonisdesuvikdo@gmail.com>
2023-05-07 21:04:06 -0700

    added setBaseDir method

--- utils/helpers/setBaseDir.js
+++ utils/helpers/setBaseDir.js
@@ -1,6 +1,14 @@
 const { resolve } = require('path');
+const commands = require('../../commands');
+
 module.exports = function setBaseDir(prompt, cmd) {
-	return prompt.length > 0
-		? resolve(`src/${cmd}s/${prompt.at(0)}`)
-		: resolve(`src/${cmd}s`);
+	if (prompt.length <= 0) return resolve(`src/${cmd}s`);
+
+	const target = prompt.at(0);
+
+	if (commands.has(target.substring(0, target.length - 1))) {
+		return resolve(`src/${target}`);
+	}
+
+	return resolve(`src/${cmd}s/${target}`);
 };

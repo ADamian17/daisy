3259dc2e03a0c377f13ec8dcd05402a894d0a2b8
ADamian17 <adonisdesuvikdo@gmail.com>
2023-05-29 10:42:09 -0700

    added StyleFile helper class

---
+++ helpers/StyleFile/__test__/mockdata.js
@@ -0,0 +1,26 @@
+const dataScssMod = {
+	file: 'Button.module.scss',
+	fileImport: `import styles from "./Button.module.scss"`
+};
+
+const dataWithoutScssMod = {
+	file: 'Button.scss',
+	fileImport: `import "./Button.scss"`
+};
+
+const dataCss = {
+	file: 'Button.module.css',
+	fileImport: `import styles from "./Button.module.css"`
+};
+
+const dataWithoutCssMod = {
+	file: 'Button.css',
+	fileImport: `import "./Button.css"`
+};
+
+module.exports = {
+	dataCss,
+	dataScssMod,
+	dataWithoutCssMod,
+	dataWithoutScssMod
+};

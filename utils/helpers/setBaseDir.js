8589d919469f914ebcbc4a9cf8e3517ac708c491
ADamian17 <adonisdesuvikdo@gmail.com>
2023-12-04 11:20:18 -0800

    check if src dir exists

--- utils/helpers/setBaseDir.js
+++ utils/helpers/setBaseDir.js
@@ -1,8 +1,10 @@
 const { resolve } = require('path');
+const { existsSync } = require('fs');
 const commands = require('../../commands');
 
 module.exports = function setBaseDir(cmd, target) {
-	const basePath = `src/${cmd}s`;
+	const srcDir = existsSync('src') ? 'src/' : '';
+	const basePath = `${srcDir}${cmd}s`;
 	if (target.toLowerCase() === 'not set') return resolve(basePath);
 
 	if (commands.has(target.substring(0, target.length - 1))) {
